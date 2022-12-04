f243ee95c87c01e866c4d8f99ac2c20022355791
9elt <nelt.primary@gmail.com>
2022-12-04 13:07:21 +0100

    fix get query

--- src/controller.rs
+++ src/controller.rs
@@ -1,15 +1,26 @@
 use axum::{extract::Path, extract::Query, Json};
 use hyper::StatusCode;
 use serde_json::{json, Value};
+use serde::{Deserialize};
 
 use crate::models::user_model;
 
+#[derive(Deserialize)]
+pub struct ModelQuery {
+    reload: Option<bool>
+}
+
 pub async fn get_user_model(
     Path(user): Path<String>,
-    reload: Query<bool>,
+    qry: Query<ModelQuery>,
 ) -> Result<Json<Value>, StatusCode> {
     let user: String = user.to_lowercase();
-    let reload: bool = reload.0;
+
+    let reload: bool = match  qry.0.reload {
+        Some(val) => val,
+        None => false
+    };
+
     match user_model::get_user_model(user, reload).await {
         Ok(model) => Ok(Json(json!(model))),
         Err(error) => Err(StatusCode::from_u16(error).unwrap()),
