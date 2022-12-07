2eebec58e388905349951fd78949254cf7a76918
9elt <nelt.primary@gmail.com>
2022-12-07 09:57:38 +0100

    calc affinity model

--- src/models/recommendations/mod.rs
+++ src/models/recommendations/mod.rs
@@ -2,56 +2,11 @@ mod affinity;
 
 use crate::helper;
 
+type Model = Vec<Vec<[i32; 9]>>;
+
 pub fn get_user_recommendations(model: [Vec<Vec<[i32; 9]>>; 2], user: &String) -> Result<Vec<String>, u16> {
 
-    let mut gte = affinity::model();
-    let mut lte = affinity::model();
-
-    let model_values = &model[0];
-    let model_avgs = &model[1];
-
-    let score_is_relavant = model_values[0][0][3] > 250;
-
-    gte[0][0][0] = model_values[0][0][0] - 300;
-    lte[0][0][0] = 100 + model_values[0][0][0] * 4;
-
-    // general stats
-    if score_is_relavant {
-        gte[0][0][1] = model_values[0][0][1] - 25;
-        lte[0][0][1] = model_values[0][0][1] + 25;
-
-        gte[0][0][2] = model_values[0][0][2] - 15;
-        lte[0][0][2] = model_values[0][0][2] + 15;
-    }
-
-    //  general Statuses stats
-
-    //  for y in 1..gte[0].len() {
-    //      gte[0][y][0] = model_values[0][y][0] - 100;
-    //      lte[0][y][0] = model_values[0][y][0] + 100;
-    //  }
-
-    //  detailed stats
-    for x in 1..gte.len() {
-        for y in 0..gte[x].len() {
-
-            if (model_avgs[x][y][0] > 60 && model_values[x][y][0] > 3)
-                || (model_avgs[x][y][0] > 25 && model_values[x][y][0] > 10)
-                || (model_avgs[x][y][0] < -100 && model_values[x][y][0] > 3)
-                || (model_avgs[x][y][0] < -25 && model_values[x][y][0] > 10)
-            {
-                gte[x][y][0] = model_values[x][y][0] - 150;
-                lte[x][y][0] = model_values[x][y][0] + 150;
-
-                if score_is_relavant {
-                    gte[x][y][1] = model_values[x][y][1] - 150;
-                    lte[x][y][2] = model_values[x][y][2] + 150;
-                }
-            }
-        }
-    }
-
-    let affinity_model = [gte, lte];
+    let affinity_model = calc_affinity_model(&model[0], &model[1]);
 
     match helper::get_affinity_users(affinity_model, user) {
         Ok(v) => Ok(v),
@@ -59,4 +14,51 @@ pub fn get_user_recommendations(model: [Vec<Vec<[i32; 9]>>; 2], user: &String) -
     }
 }
 
-fn calc_affinity_model() {}
+fn calc_affinity_model(values: &Model, avgs: &Model) -> [Model; 2] {
+
+    let mut gte: Model = affinity::model();
+    let mut lte: Model = affinity::model();
+
+    let score_is_relavant = values[0][0][3] > 250;
+
+    gte[0][0][0] = values[0][0][0] - 300;
+    lte[0][0][0] = 100 + values[0][0][0] * 4;
+
+    // general stats
+    if score_is_relavant {
+        gte[0][0][1] = values[0][0][1] - 25;
+        lte[0][0][1] = values[0][0][1] + 25;
+
+        gte[0][0][2] = values[0][0][2] - 15;
+        lte[0][0][2] = values[0][0][2] + 15;
+    }
+
+    //  general Statuses stats
+
+    //  for y in 1..gte[0].len() {
+    //      gte[0][y][0] = values[0][y][0] - 100;
+    //      lte[0][y][0] = values[0][y][0] + 100;
+    //  }
+
+    //  detailed stats
+    for x in 1..gte.len() {
+        for y in 0..gte[x].len() {
+
+            if (avgs[x][y][0] > 60 && values[x][y][0] > 3)
+                || (avgs[x][y][0] > 25 && values[x][y][0] > 10)
+                || (avgs[x][y][0] < -100 && values[x][y][0] > 3)
+                || (avgs[x][y][0] < -25 && values[x][y][0] > 10)
+            {
+                gte[x][y][0] = values[x][y][0] - 150;
+                lte[x][y][0] = values[x][y][0] + 150;
+
+                if score_is_relavant {
+                    gte[x][y][1] = values[x][y][1] - 150;
+                    lte[x][y][2] = values[x][y][2] + 150;
+                }
+            }
+        }
+    }
+
+    [gte, lte]
+}
