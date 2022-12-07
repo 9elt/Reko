726add8245623b761264745a1e0827aac02e6fb9
9elt <nelt.primary@gmail.com>
2022-12-07 11:49:34 +0100

    AffinityModel struct

--- src/models/recommendations/mod.rs
+++ src/models/recommendations/mod.rs
@@ -4,61 +4,83 @@ use crate::helper;
 
 type Model = Vec<Vec<[i32; 9]>>;
 
-pub fn get_user_recommendations(model: [Vec<Vec<[i32; 9]>>; 2], user: &String) -> Result<Vec<String>, u16> {
+struct AffinityModel<'a> {
+    gte: Model,
+    lte: Model,
+    values: &'a Model,
+    avgs: &'a Model,
+    is_score_relevant: bool,
+}
 
-    let affinity_model = calc_affinity_model(&model[0], &model[1]);
+pub fn get_user_recommendations(
+    model: [Vec<Vec<[i32; 9]>>; 2],
+    user: &String,
+) -> Result<Vec<String>, u16> {
+    let mut affinity_model: AffinityModel = AffinityModel::new(&model[0], &model[1]);
 
-    match helper::get_affinity_users(affinity_model, user) {
+    match helper::get_affinity_users(affinity_model.calc().get(), user) {
         Ok(v) => Ok(v),
-        Err(_) => Err(500)
+        Err(_) => Err(500),
     }
 }
 
-fn calc_affinity_model(values: &Model, avgs: &Model) -> [Model; 2] {
-
-    let mut gte: Model = affinity::model();
-    let mut lte: Model = affinity::model();
-
-    let score_is_relavant = values[0][0][3] > 250;
-
-    gte[0][0][0] = values[0][0][0] - 300;
-    lte[0][0][0] = 100 + values[0][0][0] * 4;
-
-    // general stats
-    if score_is_relavant {
-        gte[0][0][1] = values[0][0][1] - 25;
-        lte[0][0][1] = values[0][0][1] + 25;
-
-        gte[0][0][2] = values[0][0][2] - 15;
-        lte[0][0][2] = values[0][0][2] + 15;
-    }
-
-    //  general Statuses stats
-
-    //  for y in 1..gte[0].len() {
-    //      gte[0][y][0] = values[0][y][0] - 100;
-    //      lte[0][y][0] = values[0][y][0] + 100;
-    //  }
-
-    //  detailed stats
-    for x in 1..gte.len() {
-        for y in 0..gte[x].len() {
-
-            if (avgs[x][y][0] > 60 && values[x][y][0] > 3)
-                || (avgs[x][y][0] > 25 && values[x][y][0] > 10)
-                || (avgs[x][y][0] < -100 && values[x][y][0] > 3)
-                || (avgs[x][y][0] < -25 && values[x][y][0] > 10)
-            {
-                gte[x][y][0] = values[x][y][0] - 150;
-                lte[x][y][0] = values[x][y][0] + 150;
-
-                if score_is_relavant {
-                    gte[x][y][1] = values[x][y][1] - 150;
-                    lte[x][y][2] = values[x][y][2] + 150;
-                }
-            }
+impl<'a> AffinityModel<'a> {
+    fn new(values: &'a Model, avgs: &'a Model) -> Self {
+        Self {
+            gte: affinity::model(),
+            lte: affinity::model(),
+            values,
+            avgs,
+            is_score_relevant: false,
         }
     }
 
-    [gte, lte]
+    fn get(&'a mut self) -> [Model; 2] {
+        [self.gte.to_owned(), self.lte.to_owned()]
+    }
+
+    fn calc(&'a mut self) -> &mut Self {
+        self.calc_relevance()
+            .calc_general_stats()
+            .calc_detailed_stats()
+    }
+
+    fn calc_relevance(&'a mut self) -> &mut Self {
+        self.is_score_relevant = self.values[0][0][3] > 250;
+        self
+    }
+
+    fn calc_general_stats(&'a mut self) -> &mut Self {
+        self.gte[0][0][0] = self.values[0][0][0] - 300;
+        self.lte[0][0][0] = 100 + self.values[0][0][0] * 4;
+        if self.is_score_relevant {
+            self.gte[0][0][1] = self.values[0][0][1] - 25;
+            self.lte[0][0][1] = self.values[0][0][1] + 25;
+
+            self.gte[0][0][2] = self.values[0][0][2] - 15;
+            self.lte[0][0][2] = self.values[0][0][2] + 15;
+        }
+        self
+    }
+
+    fn calc_detailed_stats(&'a mut self) -> &mut Self {
+        for x in 1..self.gte.len() {
+            for y in 0..self.gte[x].len() {
+                if (self.avgs[x][y][0] > 60 && self.values[x][y][0] > 3)
+                    || (self.avgs[x][y][0] > 25 && self.values[x][y][0] > 10)
+                    || (self.avgs[x][y][0] < -100 && self.values[x][y][0] > 3)
+                    || (self.avgs[x][y][0] < -25 && self.values[x][y][0] > 10)
+                {
+                    self.gte[x][y][0] = self.values[x][y][0] - 150;
+                    self.lte[x][y][0] = self.values[x][y][0] + 150;
+
+                    if self.is_score_relevant {
+                        self.gte[x][y][1] = self.values[x][y][1] - 150;
+                        self.lte[x][y][2] = self.values[x][y][2] + 150;
+                    }
+                }
+            }
+        }
+        self
+    }
 }
