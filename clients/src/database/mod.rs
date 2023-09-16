2a676d209413538663056a58ef59c430bdbc9d79
9elt <nelt.primary@gmail.com>
2023-09-16 13:59:26 +0200

    diesel setup

--- clients/src/database/mod.rs
+++ clients/src/database/mod.rs
@@ -6,6 +6,5 @@ use std::env;
 fn connect() -> MysqlConnection {
     let database_url = env::var("DATABASE_URL").expect("missing env variable DATABASE_URL");
 
-    MysqlConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+    MysqlConnection::establish(&database_url).expect("Cannot connect to database")
 }
