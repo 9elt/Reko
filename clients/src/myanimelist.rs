0c470a34f3d477a64e7aaf7ce587b5f9166e25f2
9elt <nelt.primary@gmail.com>
2023-09-17 10:41:32 +0200

    fix mal list

--- clients/src/myanimelist.rs
+++ clients/src/myanimelist.rs
@@ -111,8 +111,10 @@ impl MALClient {
                 .filter(|e| !is_update || e.updated_at > updated_at)
                 .collect();
 
-            if raw.paging.next.is_some() && entries.len() > 0 {
-                res.append(&mut entries);
+            let found = entries.len();
+            res.append(&mut entries);
+
+            if raw.paging.next.is_some() && found > 0 {
                 offset += 1;
             } else {
                 break;
@@ -323,24 +325,24 @@ impl Stat {
         let year = date.year();
 
         if year < 1990 {
-            Self(0)
+            Self(61)
         } else if year < 2000 {
-            Self(1)
+            Self(40)
         } else if year < 2010 {
-            Self(2)
+            Self(14)
         } else if year < 2018 {
-            Self(3)
+            Self(1)
         } else {
-            Self(4)
+            Self(6)
         }
     }
     fn from_series_len(num_episodes: i32) -> Self {
         if num_episodes == 1 {
-            Self(5)
+            Self(16)
         } else if num_episodes < 9 {
-            Self(6)
+            Self(33)
         } else if num_episodes < 19 {
-            Self(7)
+            Self(0)
         } else if num_episodes < 33 {
             Self(8)
         } else {
@@ -349,93 +351,93 @@ impl Stat {
     }
     fn from_rating(rating: String) -> Self {
         match rating.as_str() {
-            "g" => Self(10),
-            "pg" => Self(11),
-            "pg_13" => Self(12),
-            "r" => Self(13),
-            "r+" => Self(14),
-            "rx" => Self(15),
+            "g" => Self(50),
+            "pg" => Self(59),
+            "pg_13" => Self(11),
+            "r" => Self(7),
+            "r+" => Self(24),
+            "rx" => Self(72),
             _ => Self(-1),
         }
     }
     fn from_genre_id(genre_id: i32) -> Self {
         match genre_id {
-            1 => Self(16),  // Action
-            2 => Self(17),  // Adventure
-            4 => Self(18),  // Comedy
-            8 => Self(19),  // Drama
-            10 => Self(20), // Fantasy
-            22 => Self(21), // Romance
-            24 => Self(22), // SciFi
-            37 => Self(23), // Supernatural
-            5 => Self(24),  // AvantGarde
+            1 => Self(2),   // Action
+            2 => Self(13),  // Adventure
+            4 => Self(4),   // Comedy
+            8 => Self(9),   // Drama
+            10 => Self(3),  // Fantasy
+            22 => Self(10), // Romance
+            24 => Self(17), // SciFi
+            37 => Self(15), // Supernatural
+            5 => Self(70),  // AvantGarde
             46 => Self(25), // AwardWinning
-            28 => Self(26), // BoysLove
-            26 => Self(27), // GirlsLove
-            47 => Self(28), // Gourmet
-            14 => Self(29), // Horror
-            7 => Self(30),  // Mystery
-            36 => Self(31), // SliceofLife
-            30 => Self(32), // Sports
-            41 => Self(33), // Suspense
-            9 => Self(34),  // Ecchi
-            49 => Self(35), // Erotica
-            12 => Self(36), // Hentai
-            43 => Self(37), // Josei
-            15 => Self(38), // Kids
-            42 => Self(39), // Seinen
-            25 => Self(40), // Shoujo
-            27 => Self(41), // Shounen
-            50 => Self(42), // AdultCast
-            57 => Self(43), // GagHumor
-            58 => Self(44), // Gore
-            35 => Self(45), // Harem
-            13 => Self(46), // Historical
-            62 => Self(47), // Isekai
-            63 => Self(48), // Iyashikei
-            64 => Self(49), // LovePolygon
-            17 => Self(50), // MartialArts
-            18 => Self(51), // Mecha
-            38 => Self(52), // Military
-            19 => Self(53), // Music
-            6 => Self(54),  // Mythology
-            20 => Self(55), // Parody
-            40 => Self(56), // Psychological
-            23 => Self(57), // School
-            31 => Self(58), // SuperPower
-            76 => Self(59), // Survival
-            78 => Self(60), // TimeTravel
-            32 => Self(61), // Vampire
-            51 => Self(62), // Anthropomorphic
-            52 => Self(63), // CGDCT
-            53 => Self(64), // Childcare
-            54 => Self(65), // CombatSports
-            81 => Self(66), // Crossdressing
-            55 => Self(67), // Delinquents
-            39 => Self(68), // Detective
-            56 => Self(69), // Educational
-            59 => Self(70), // HighStakesGame
-            60 => Self(71), // IdolsFemale
-            61 => Self(72), // IdolsMale
-            65 => Self(73), // MagicalSexShift
-            66 => Self(74), // MahouShoujo
-            67 => Self(75), // Medical
-            68 => Self(76), // OrganizedCrime
-            69 => Self(77), // OtakuCulture
-            70 => Self(78), // PerformingArts
-            71 => Self(79), // Pets
-            3 => Self(80),  // Racing
-            72 => Self(81), // Reincarnation
-            73 => Self(82), // ReverseHarem
-            74 => Self(83), // RomanticSubtext
-            21 => Self(84), // Samurai
-            75 => Self(85), // Showbiz
-            29 => Self(86), // Space
-            11 => Self(87), // StrategyGame
-            77 => Self(88), // TeamSports
-            79 => Self(89), // VideoGame
-            80 => Self(90), // VisualArts
-            48 => Self(91), // Workplace
+            28 => Self(76), // BoysLove
+            26 => Self(77), // GirlsLove
+            47 => Self(68), // Gourmet
+            14 => Self(36), // Horror
+            7 => Self(19),  // Mystery
+            36 => Self(39), // SliceofLife
+            30 => Self(41), // Sports
+            41 => Self(29), // Suspense
+            9 => Self(21),  // Ecchi
+            49 => Self(86), // Erotica
+            12 => Self(71), // Hentai
+            43 => Self(74), // Josei
+            15 => Self(73), // Kids
+            42 => Self(18), // Seinen
+            25 => Self(38), // Shoujo
+            27 => Self(5),  // Shounen
+            50 => Self(28), // AdultCast
+            57 => Self(47), // GagHumor
+            58 => Self(26), // Gore
+            35 => Self(27), // Harem
+            13 => Self(32), // Historical
+            62 => Self(30), // Isekai
+            63 => Self(53), // Iyashikei
+            64 => Self(43), // LovePolygon
+            17 => Self(49), // MartialArts
+            18 => Self(35), // Mecha
+            38 => Self(31), // Military
+            19 => Self(45), // Music
+            6 => Self(34),  // Mythology
+            20 => Self(42), // Parody
+            40 => Self(20), // Psychological
+            23 => Self(8),  // School
+            31 => Self(22), // SuperPower
+            76 => Self(37), // Survival
+            78 => Self(44), // TimeTravel
+            32 => Self(46), // Vampire
+            51 => Self(69), // Anthropomorphic
+            52 => Self(57), // CGDCT
+            53 => Self(67), // Childcare
+            54 => Self(82), // CombatSports
+            81 => Self(85), // Crossdressing
+            55 => Self(78), // Delinquents
+            39 => Self(55), // Detective
+            56 => Self(89), // Educational
+            59 => Self(64), // HighStakesGame
+            60 => Self(81), // IdolsFemale
+            61 => Self(90), // IdolsMale
+            65 => Self(88), // MagicalSexShift
+            66 => Self(66), // MahouShoujo
+            67 => Self(87), // Medical
+            68 => Self(60), // OrganizedCrime
+            69 => Self(52), // OtakuCulture
+            70 => Self(79), // PerformingArts
+            71 => Self(91), // Pets
+            3 => Self(84),  // Racing
+            72 => Self(48), // Reincarnation
+            73 => Self(80), // ReverseHarem
+            74 => Self(51), // RomanticSubtext
+            21 => Self(63), // Samurai
+            75 => Self(83), // Showbiz
+            29 => Self(62), // Space
+            11 => Self(65), // StrategyGame
+            77 => Self(54), // TeamSports
+            79 => Self(56), // VideoGame
+            80 => Self(75), // VisualArts
+            48 => Self(58), // Workplace
             _ => Self(-1),
         }
     }
